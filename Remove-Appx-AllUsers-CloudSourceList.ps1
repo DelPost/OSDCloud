49288ed4560a8ecc4d0733383df35de12c0d1d1e
DelPost <hpost123@yahoo.dk>
2026-03-20 09:08:11 +0100

    Remove-Appx-AllUsers-CloudSourceList.ps1

--- Remove-Appx-AllUsers-CloudSourceList.ps1
+++ Remove-Appx-AllUsers-CloudSourceList.ps1
@@ -1,3 +1,20 @@
+# removeappx.osdcloud.ch
+$Title = "Remove-Appx-AllUsers-CloudSourceList"
+$host.UI.RawUI.WindowTitle = $Title
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+$env:APPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Roaming"
+$env:LOCALAPPDATA = "C:\Windows\System32\Config\SystemProfile\AppData\Local"
+$Env:PSModulePath = $env:PSModulePath+";C:\Program Files\WindowsPowerShell\Scripts"
+$env:Path = $env:Path+";C:\Program Files\WindowsPowerShell\Scripts"
+
+$Global:Transcript = "$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-AddToAutopilot.log"
+Start-Transcript -Path (Join-Path "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD\" $Global:Transcript) -ErrorAction Ignore
+
+Write-Host "Removing Appx AllUsers From CloudSourceList" -ForegroundColor Green
+
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+
 
 Begin {
 
@@ -173,3 +190,5 @@ Process {
         Write-LogEntry -Value "No Black List Apps defined in array"
     }
 }
+
+Stop-Transcript
